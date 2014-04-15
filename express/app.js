28061e71fe49fe1136deae44e81068a5a3239a63
dougmolineux <doug.molineux@gmail.com>
2014-04-14 19:44:59 -0700

    Updated app.js with api

--- express/app.js
+++ express/app.js
@@ -3,13 +3,27 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express'),
+    routes = require('./routes'),
+    mongoose = require('../node_modules/mongoose');
 
 var app = module.exports = express.createServer();
 
-// Configuration
+// connect to wc mongodb
+mongoose.connect('mongodb://localhost/wc');
 
+// define the schema for a team
+var teamSchema = new mongoose.Schema({
+        name: String,
+        continent: String,
+        group: String,
+        ranking: Number,
+});
+
+// create the model from the schema
+var Team = mongoose.model('Team', teamSchema);
+
+// Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -24,13 +38,15 @@ app.configure('development', function(){
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 // Routes
-
 app.get('/', routes.index);
+app.get('/api', function (req, res) {
+	res.send('WC2014 API is running');
+});
 
 app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
